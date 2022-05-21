2e429742ddf3413cb88c612d83e603485c8616eb
House of code <houseofcode@Houses-MacBook-Pro.local>
2022-05-21 08:32:17 +0200

    Changes made to make the app work

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {StyleSheet} from 'react-native';
 import auth from '@react-native-firebase/auth';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
--- components/SignIn.js
+++ components/SignIn.js
@@ -2,7 +2,6 @@ import React, {useState} from 'react';
 import {
   Text,
   SafeAreaView,
-  Button,
   TextInput,
   StyleSheet,
   TouchableOpacity,
